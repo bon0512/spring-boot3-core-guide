018275daa6cb1962f059e2dbd3bc0f38666f1602
bon0512 <nonstop00450@gmail.com>
2025-11-27 15:15:48 +0900

    practice

---
+++ src/main/java/com/example/hello/entity/Producer.java
@@ -0,0 +1,36 @@
+package com.example.hello.entity;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@Table(name = "producer")
+public class Producer  extends BaseEntity{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String code;
+
+    private String name;
+
+    @ManyToMany
+    @ToString.Exclude
+    private List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product){
+        products.add(product);
+    }
+
+
+}
